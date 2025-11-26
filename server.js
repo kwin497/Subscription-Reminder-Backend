88f62da9eefa7b60c80e1cc66bdf055678c95b0c
kwin497 <glorykibe73@gmail.com>
2025-11-26 14:39:58 +0300

    Update CRON job schedule time

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // CRON JOB: Runs every day at 8 AM
-cron.schedule("52 10 * * *", async () => {
+cron.schedule("44 14 * * *", async () => {
   console.log("⏰ Checking for upcoming renewals...");
 
   const now = new Date();
@@ -72,3 +72,4 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 
 });
+
