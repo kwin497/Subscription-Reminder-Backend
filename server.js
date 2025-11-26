0df0647925ff5d39686bc59bf6e21acf632020b6
kwin497 <glorykibe73@gmail.com>
2025-11-26 19:36:05 +0300

    Fix cron job schedule time to 8 PM

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // CRON JOB: Runs every day at 8 AM
-cron.schedule("35 19 * * *", async () => {
+cron.schedule("00 20 * * *", async () => {
   console.log("⏰ Checking for upcoming renewals...");
 
   const now = new Date();
@@ -72,6 +72,7 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 
 });
+
 
 
 
